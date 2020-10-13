ef5d08aada13ba9149e8e27e6d44d5a165502dbb
魏雷鸣 <weileiming@wasu.tv>
2020-10-13 09:38:23 +0800

    daily update

---
+++ Universal_Framework/src/referenceComponent/脚本DEMO.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name 	 	 百度搜索
+// @namespace    https://www.baidu.com/
+// @description  随时可以进行百度搜索
+// @version      0.0.6
+// @match        *://www.baidu.com/*
+// @icon	 http://7xo0rb.com1.z0.glb.clouddn.com/public/16-12-5/39527384.jpg
+// @author       weileiming
+// @licence      WTFPL
+// @grant        none
+// @encoding     utf-8
+// @date         2/12/2016
+// ==/UserScript==
+window.onload = function() {
+   var btn = document.createElement("button")
+   btn.onclick = function(){
+   }
+}
